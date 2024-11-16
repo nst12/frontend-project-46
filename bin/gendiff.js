56ed9a49944b934fb857a7df9b27bdf6846873a6
Anastasia <reginald.von.ravenhorst01@yandex.ru>
2024-11-16 17:57:46 +0300

    first step

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import  { Command } from "commander";
+
+const program = new Command();
+
+program
+  .version('1.0.0', '-V, --version')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
