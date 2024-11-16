5821a6414c45a6b092c3ee4cc4b6005da3ec520c
Anastasia <reginald.von.ravenhorst01@yandex.ru>
2024-11-16 18:06:59 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const program = new Command();
 program
   .version('1.0.0', '-V, --version')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
