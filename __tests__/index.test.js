0109889d813999615c3ab0a29b44dbb745fefe31
Anastasia <reginald.von.ravenhorst01@yandex.ru>
2024-11-24 17:49:10 +0300

    refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,60 +11,40 @@ const getGenDiffFixture = (filePath1, filePath2, expectedFilePath) => {
   };
 };
 
-test('getDiffJson', () => {
-  const fixture = getGenDiffFixture(
-    'file1.json',
-    'file2.json',
-    'formatResult.txt',
-  );
-  expect(getDiff(fixture.filePath1, fixture.filePath2)).toBe(fixture.expected);
-});
+const runTest = (description, file1, file2, expected, format) => {
+  test(description, () => {
+    const fixture = getGenDiffFixture(file1, file2, expected);
+    expect(getDiff(fixture.filePath1, fixture.filePath2, format)).toBe(
+      fixture.expected,
+    );
+  });
+};
 
-test('getDiffYaml', () => {
-  const fixture = getGenDiffFixture(
-    'file1.yml',
-    'file2.yml',
-    'formatResult.txt',
-  );
-  expect(getDiff(fixture.filePath1, fixture.filePath2)).toBe(fixture.expected);
-});
-
-test('getDiffJsonNested', () => {
-  const fixture = getGenDiffFixture(
-    'file1nested.json',
-    'file2nested.json',
-    'formatResultNested.txt',
-  );
-  expect(getDiff(fixture.filePath1, fixture.filePath2)).toBe(fixture.expected);
-});
-
-test('getDiffYamlNested', () => {
-  const fixture = getGenDiffFixture(
-    'file1nested.yaml',
-    'file2nested.yaml',
-    'formatResultNested.txt',
-  );
-  expect(getDiff(fixture.filePath1, fixture.filePath2)).toBe(fixture.expected);
-});
-
-test('getDiffJsonNestedFormatPlain', () => {
-  const fixture = getGenDiffFixture(
-    'file1nested.json',
-    'file2nested.json',
-    'formatResultNestedPlain.txt',
-  );
-  expect(getDiff(fixture.filePath1, fixture.filePath2, 'plain')).toBe(
-    fixture.expected,
-  );
-});
-
-test('getDiffJsonNestedFormatJson', () => {
-  const fixture = getGenDiffFixture(
-    'file1nested.json',
-    'file2nested.json',
-    'formatResultNestedJson.txt',
-  );
-  expect(getDiff(fixture.filePath1, fixture.filePath2, 'json')).toBe(
-    fixture.expected,
-  );
-});
+runTest('getDiffJson', 'file1.json', 'file2.json', 'formatResult.txt');
+runTest('getDiffYaml', 'file1.yml', 'file2.yml', 'formatResult.txt');
+runTest(
+  'getDiffJsonNested',
+  'file1nested.json',
+  'file2nested.json',
+  'formatResultNested.txt',
+);
+runTest(
+  'getDiffYamlNested',
+  'file1nested.yaml',
+  'file2nested.yaml',
+  'formatResultNested.txt',
+);
+runTest(
+  'getDiffJsonNestedFormatPlain',
+  'file1nested.json',
+  'file2nested.json',
+  'formatResultNestedPlain.txt',
+  'plain',
+);
+runTest(
+  'getDiffJsonNestedFormatJson',
+  'file1nested.json',
+  'file2nested.json',
+  'formatResultNestedJson.txt',
+  'json',
+);
